4a119f65f2e54eb2e873a94ce89276d8071f2454
Stefany Mojica <stefymojica40@gmail.com>
2023-03-31 19:52:07 -0500

    first commit

---
+++ js/toDo.js
@@ -0,0 +1,9 @@
+const writeNote = document.getElementById('writeNote');
+const addNewNote = document.getElementById('addNewNote');
+const noteList = document.getElementsByClassName("noteList");
+
+addNewNote.addEventListener("click", () => {
+    
+})
+
+
